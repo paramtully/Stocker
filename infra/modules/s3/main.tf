61e945d8649a28e13dcfaa7e01eba39b4cb2c71d
paramtully <ptully49@students.cs.ubc.ca>
2026-02-03 16:43:12 -0800

    disabled s3 versioning because its an intermediary data store

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
 resource "aws_s3_bucket_versioning" "bucket" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
-    status = "Enabled"
+    status = "Disabled"
   }
 }
 
